ec829b08ae8f61e5adb15a856884d1e8546bdadd
Shirley Leu <shirley.leu@schibsted.com>
2018-12-04 23:58:36 +0100

    Add solution to Part 2

--- 1/main.go
+++ 1/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/pkg/errors"
-	"io"
 	"log"
 	"os"
 	"strconv"
@@ -15,6 +14,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer input.Close()
 
 	freq, err := parse(input)
@@ -24,26 +24,31 @@ func main() {
 	fmt.Println(freq)
 }
 
-func parse(input io.Reader) (int, error) {
+func parse(input *os.File) (int, error) {
 	freq := 0
 	var m = make(map[int]int)
 	// Initialize for freq of 0
 	m[freq] += 1
-	scanner := bufio.NewScanner(input)
-	for scanner.Scan() {
-		lineStr := scanner.Text()
-		num, err := strconv.Atoi(lineStr)
-		if err != nil {
-			return 0, errors.Wrap(err, "parse input file error")
+	for {
+		input.Seek(0,0)
+		scanner := bufio.NewScanner(input)
+		for scanner.Scan() {
+			lineStr := scanner.Text()
+			num, err := strconv.Atoi(lineStr)
+			if err != nil {
+				return 0, errors.Wrap(err, "parse input file error")
+			}
+			// Change the freq
+			freq += num
+
+			// Increment the number of times that freq has been hit
+			m[freq] += 1
+
+
+			// Check the count for a freq to break
+			if m[freq] > 1 {
+				return freq, nil
+			}
 		}
-		// Change the freq
-		freq += num
-
-		// Increment the number of times that freq has been hit
-		m[freq] += 1
-
-		// check how many times this freq has been hit
-
 	}
-	return freq, nil
 }
